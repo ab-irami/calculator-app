62fa559d92d517658d4bd4c9111dff53febbe49a
Abirami Sekar <abirami.sekar1609001@gmail.com>
2021-08-14 10:48:40 +0530

    Complete Project

---
+++ README.md
@@ -0,0 +1,39 @@
+# Frontend Mentor - Calculator app solution
+
+This is a solution to the [Calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- Perform mathmatical operations like addition, subtraction, multiplication, and division
+- Adjust the color theme based on their preference
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- JS 
+
+
+### What I learned
+
+Learned a lot in this challenge
+
+## Author
+
+- Frontend Mentor - [@Provokeabi](https://www.frontendmentor.io/profile/Provokeabi)
+- Twitter - [@_abi_rami_](https://twitter.com/_abi_rami_)
+
+
---
+++ index.js
@@ -0,0 +1,116 @@
+const links = document.querySelectorAll("link");
+const toggleBtn = document.querySelectorAll("input");
+const prevOperandText = document.querySelector("[data-previous-operand]");
+const currentOperandText = document.querySelector("[data-current-operand]");
+const deleteBtn = document.querySelector("[data-delete]");
+const resultBtn = document.querySelector("[data-output]");
+const resetBtn = document.querySelector("[data-reset]");
+const operands = document.querySelectorAll("[data-num]");
+const operatorBtn = document.querySelectorAll("[data-operator]");
+let prevOperand = prevOperandText.innerText;
+let currentOperand = currentOperandText.innerText;
+let operation;
+
+function themeChange(i) {
+    if(i === "0"){
+        links[2].setAttribute("href", "");
+    } else {
+        links[2].setAttribute("href", `css/theme${i}.css`);
+    }
+}
+
+function reset() {
+    prevOperand = "";    
+    currentOperand = "";
+    operation = undefined;
+}
+
+function deleteOperand() {
+    currentOperand = currentOperand.toString().slice(0, -1);
+}
+
+function addNumber(number) {
+    if(number === "." && currentOperand.includes(".")) return;
+    currentOperand = currentOperand.toString() + number.toString();
+}
+
+function operationSelection(operate) {
+    if(currentOperandText === "") return;
+    if(prevOperandText !== "") {
+        calculatorOperation();
+    }
+    operation = operate;
+    prevOperand = currentOperand;
+    currentOperand = "";
+}
+
+function calculatorOperation() {
+    let result;
+    let prev = parseFloat(prevOperand);
+    let current = parseFloat(currentOperand);
+    if(isNaN(prev) || isNaN(current)) return;
+
+    switch(operation){
+        case "+":
+            result = prev + current;
+            break;
+
+        case "-":
+            result = prev - current 
+            break;
+
+        case "×":
+            result = prev * current; 
+            break; 
+
+        case "/":
+            result = prev / current;
+            break;
+
+        default: 
+            return;
+    } 
+    currentOperand = result;
+    operation = undefined;
+    prevOperand = "";
+}
+
+function displayNum() {
+    currentOperandText.innerText = currentOperand.toLocaleString("en");
+    prevOperandText.innerText = prevOperand;
+}
+
+toggleBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        themeChange(btn.value);
+    });
+})
+
+resetBtn.addEventListener("click", () => {
+    reset();
+    displayNum();
+});
+
+deleteBtn.addEventListener("click", () => {
+    deleteOperand();
+    displayNum();
+});
+
+operands.forEach(operand => {
+    operand.addEventListener("click", () => {
+        addNumber(operand.innerText);
+        displayNum();
+    });
+});
+    
+operatorBtn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        operationSelection(btn.innerText);
+        displayNum();        
+    })
+})
+
+resultBtn.addEventListener("click", () => {
+    calculatorOperation();
+    displayNum();
+});
